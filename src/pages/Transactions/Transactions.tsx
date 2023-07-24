86b84b61e712e59b52d0c456b0f76b1e3ca71d9f
Gabriel Jorge <gabriel.ggcj@gmail.com>
2023-07-24 17:46:17 -0300

    feature/add-transaction-table

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,11 +1,44 @@
 import { Header } from "../../components/Header/Header"
 import { Summary } from "../../components/Summary/Summary"
+import { PriceHighLigth, TransactionsContainer, TransactionsTable } from "./styles"
 
 export const Transactions = ( ) => {
     return(
         <div>
             <Header/>
             <Summary/>
+
+
+
+          <TransactionsContainer>
+          <TransactionsTable>
+                <tbody>
+
+                    <tr>
+                        <td width="40%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighLigth variant="income">
+                                    R$ 12.000,00
+                                </PriceHighLigth>                            
+                            </td>
+                        <td>Venda</td>
+                        <td>13/04/2022</td>
+                    </tr>
+
+                    <tr>
+                        <td width="40%">Hamburguer</td>
+                            <td>
+                                <PriceHighLigth variant="outcome">
+                                    - R$ 59,00
+                                </PriceHighLigth>                            
+                            </td>
+                        <td>Alimentação</td>
+                        <td>13/04/2022</td>
+                    </tr>
+                    
+                </tbody>
+            </TransactionsTable>
+          </TransactionsContainer>
         </div>
     )
 }
