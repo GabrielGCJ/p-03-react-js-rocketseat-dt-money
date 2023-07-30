71887aa7201fe017565d5358f5815e2e92fc0e51
Gabriel Jorge <gabriel.ggcj@gmail.com>
2023-07-30 01:24:00 -0300

    feature/add-HTTP-request

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,47 +1,63 @@
-import { Header } from "../../components/Header/Header"
-import { Summary } from "../../components/Summary/Summary"
-import { SearchForm } from "./components/SearchForm/SearchForm"
-import { PriceHighLigth, TransactionsContainer, TransactionsTable } from "./styles"
+import { useEffect, useState } from "react";
+import { Header } from "../../components/Header/Header";
+import { Summary } from "../../components/Summary/Summary";
+import { SearchForm } from "./components/SearchForm/SearchForm";
+import {
+  PriceHighLigth,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
-export const Transactions = ( ) => {
-    return(
-        <div>
-            <Header/>
-            <Summary/>
-
-
-
-          <TransactionsContainer>
-            <SearchForm/>
-          <TransactionsTable>
-
-                <tbody>
-
-                    <tr>
-                        <td width="40%">Desenvolvimento de site</td>
-                            <td>
-                                <PriceHighLigth variant="income">
-                                    R$ 12.000,00
-                                </PriceHighLigth>                            
-                            </td>
-                        <td>Venda</td>
-                        <td>13/04/2022</td>
-                    </tr>
-
-                    <tr>
-                        <td width="40%">Hamburguer</td>
-                            <td>
-                                <PriceHighLigth variant="outcome">
-                                    - R$ 59,00
-                                </PriceHighLigth>                            
-                            </td>
-                        <td>Alimentação</td>
-                        <td>13/04/2022</td>
-                    </tr>
-                    
-                </tbody>
-            </TransactionsTable>
-          </TransactionsContainer>
-        </div>
-    )
+interface Transaction {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  category: string;
+  price: number;
+  createdAt: string;
 }
+
+export const Transactions = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  const loadTransactions = async () => {
+    const response = await fetch("http://localhost:3000/transactions");
+    const data = await response.json();
+
+    console.log(data);
+    setTransactions(data);
+  };
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="40%">{transaction.description}</td>
+                  <td>
+                    <PriceHighLigth variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighLigth>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  );
+};
